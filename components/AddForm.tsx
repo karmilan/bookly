eff376434d1fe45f130c81bf8ad4565f55450592
karmilan <delftkarmilan@gmail.com>
2025-06-18 23:59:15 +0530

    deployment fixes

--- components/AddForm.tsx
+++ components/AddForm.tsx
@@ -97,7 +97,7 @@ const AddForm: FC<AddFormProps> = ({
           </label>
           <textarea
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
-            rows="4"
+            rows={4}
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             required
